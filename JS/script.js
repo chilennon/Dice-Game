8985789dcd3c65f1a01f94623b4feb8937d5c303
Chi <jjide21@live.com>
2025-09-04 21:30:09 +0100

    Fix JavaScript and HTML issues for GitHub Pages

--- JS/script.js
+++ JS/script.js
@@ -1,23 +1,14 @@
-
-var randomDiceFace1 = Math.floor((Math.random() * 6) + 1);
-
-var randomDiceImage1 = "images/" + "dice" + randomDiceFace1 + ".svg";
-
+// Player 1
+var randomDiceFace1 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage1 = "images/dice" + randomDiceFace1 + ".svg";
 document.querySelector(".player1image").setAttribute("src", randomDiceImage1);
 
-
-// change second dice image
-
+// Player 2
 var randomDiceFace2 = Math.floor(Math.random() * 6) + 1;
-
-var randomDiceImage2 = "images/" + "dice" + randomDiceFace2 + ".svg";
-
+var randomDiceImage2 = "images/dice" + randomDiceFace2 + ".svg";
 document.querySelector(".player2image").setAttribute("src", randomDiceImage2);
 
-
-
-// result
-
+// Decide winner (compare numbers, not strings)
 if (randomDiceFace1 > randomDiceFace2) {
     document.querySelector(".result").innerHTML = "Player 1 wins";
 } else if (randomDiceFace1 < randomDiceFace2) {
