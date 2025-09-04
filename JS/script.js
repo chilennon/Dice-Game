e5fb132bf80e6e609c5168e3b56fde0c2a6c78ba
Chi <jjide21@live.com>
2025-09-04 21:28:36 +0100

    Fix JavaScript and HTML issues for GitHub Pages

--- JS/script.js
+++ JS/script.js
@@ -15,19 +15,13 @@ var randomDiceImage2 = "images/" + "dice" + randomDiceFace2 + ".svg";
 document.querySelector(".player2image").setAttribute("src", randomDiceImage2);
 
 
-// 
-
-var randomDiceFace3 = Math.floor(Math.random() * 6) + 1;
-
-var randomDiceImage3 = "images/" + "dice" + randomDiceFace3 + "svg";
-
 
 // result
 
-if (randomDiceImage1 > randomDiceImage2) {
+if (randomDiceFace1 > randomDiceFace2) {
     document.querySelector(".result").innerHTML = "Player 1 wins";
-} else if (randomDiceImage1 < randomDiceImage2) {
+} else if (randomDiceFace1 < randomDiceFace2) {
     document.querySelector(".result").innerHTML = "Player 2 wins";
-} else if (randomDiceImage1 === randomDiceImage2) {
-    document.querySelector(".result").innerHTML = "Its a tie!";
+} else {
+    document.querySelector(".result").innerHTML = "It's a tie!";
 }
